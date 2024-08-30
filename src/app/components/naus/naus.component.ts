f517bd56c5c3cbdb9251f0632215c165af3fdea3
Xavier Prat <xavierprat78@gmail.com>
2024-08-30 12:27:12 +0200

    7.2 repassat

--- src/app/components/naus/naus.component.ts
+++ src/app/components/naus/naus.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core'; //signal variable reactives 
 import { ServeiNausService } from '../../sevices/servei.naus.service';
 import { Nau } from '../../interfaces/nau';
 import { CommonModule } from '@angular/common';
@@ -11,22 +11,22 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./naus.component.scss']
 })
 export class NausComponent implements OnInit {
-  naus = signal<Nau[]>([]);
-  nauSeleccionada = signal<Nau | null>(null);
-  nauImatgeUrl = signal<string | null>(null);
+  naus = signal<Nau[]>([]); //guardem llista de naus recuperada de l'API, inicialment està buit
+  nauSeleccionada = signal<Nau | null>(null); //guardem la nau seleccionada per l'usuari. Iniciem com null
+  nauImatgeUrl = signal<string | null>(null); //guardem al Url de la nau seleccionada. Iniciem com null
 
-  constructor(private nausService: ServeiNausService) { }
+  constructor(private nausService: ServeiNausService) { } // injectem el servei per utilitzar-lo
 
-  ngOnInit(): void {
-    this.nausService.getNaus().subscribe((data: Nau[]) => {
-      this.naus.set(data); // Ara 'data' és un array de naus
+  ngOnInit(): void { // S'executa quan el component s'inicialitza. Obté les naus i quan es carreguen s'assignen al signal "naus"
+    this.nausService.getNaus().subscribe((data: Nau[]) => {//rebem les dades de l'observable a data
+      this.naus.set(data); // Assignem les dades obtingudes a data al signal naus amb el mètode set() això actualitza el signal
       console.log('Naus carregades:', this.naus());
     });
   }
 
-  showNauDetail(nau: Nau): void {
+  mostraDetallNau(nau: Nau): void {//El mètode rebrà l'objecte Nau
     this.nauSeleccionada.set(nau);
-    const nauId = nau.url.split('/').filter(Boolean).pop();
+    const nauId = nau.url.split('/').filter(Boolean).pop();//dividim, filtrem cadenes buides i agafem l'última
     this.nauImatgeUrl.set(`https://starwars-visualguide.com/assets/img/starships/${nauId}.jpg`);
   }
 }
--- src/app/sevices/servei.naus.service.ts
+++ src/app/sevices/servei.naus.service.ts
@@ -1,20 +1,20 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { Nau } from '../interfaces/nau';
+import { HttpClient } from '@angular/common/http'; //per fer peticions http get, post, etc.
+import { Injectable } from '@angular/core'; //marca una calsse com a servei 
+import { Observable } from 'rxjs'; // permet gestionar dades asíncrones com les dades que arriben de la petició http 
+import { map } from 'rxjs/operators'; // permet transofrmar i extreure les dades que arriben de l'observable 
+import { Nau } from '../interfaces/nau'; // interfície
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServeiNausService {
-  private apiUrl = 'https://swapi.dev/api/starships';
+  private apiUrl = 'https://swapi.dev/api/starships'; // si és privada només s'utilitza en aquesta classe
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { } // permet fer peticions http dins de la classe 
 
   getNaus(): Observable<Nau[]> {
     return this.http.get<{ results: Nau[] }>(this.apiUrl).pipe(
-      map(response => response.results) // Aquí extraiem l'array de 'results'
+      map(response => response.results) // Amb map treiem de la resposta de l'api (reponse) només l'array de naus 
     );
   }
 }
